cf6bcfe0a0f9331c496bee6e3c7657a6b7cb7223
daniel.knezevic <daniel.knezevic@NZ162>
2016-07-12 14:25:38 +1200

    committing via GUI

--- server/server/main.cpp
+++ server/server/main.cpp
@@ -1,7 +1,97 @@
 #include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <ctime>
+
+using boost::asio::ip::tcp;
+
+std::string makeDaytimeString()
+{
+    std::time_t now = std::time(0);
+    return std::ctime(&now);
+}
+
+class TcpConnection : public boost::enable_shared_from_this<TcpConnection>
+{
+public:
+    typedef boost::shared_ptr<TcpConnection> pointer;
+    static pointer create(boost::asio::io_service& io)
+    {
+        std::cout << "creating TcpConnection pointer" << std::endl;
+        return pointer(new TcpConnection(io));
+    }
+
+    tcp::socket& socket()
+    {
+        return mSocket;
+    }
+
+    void Start()
+    {
+        std::cout << "Starting" << std::endl;
+        mMessage = "asdfasdfdsaf";
+        std::cout << "Message: " << mMessage << std::endl;
+        boost::asio::async_write(mSocket, boost::asio::buffer(mMessage), boost::bind(&TcpConnection::HandleWrite, shared_from_this(), boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+    }
+
+private:
+    TcpConnection(boost::asio::io_service& io)
+        : mSocket(io)
+    {
+
+    }
+
+    void HandleWrite(const boost::system::error_code& error, size_t bytesTransferred)
+    {
+        std::cout << "Handling write: error=\"" << error.message() << "\"; bytesTransferred: " << bytesTransferred;
+    }
+
+    tcp::socket mSocket;
+    std::string mMessage;
+};
+
+class TcpServer
+{
+public:
+    TcpServer(boost::asio::io_service& io)
+        : mAcceptor(io, tcp::endpoint(tcp::v4(), 45555))
+    {
+        StartAccept();
+    }
+
+private:
+    void StartAccept()
+    {
+        std::cout << "Starting accept" << std::endl;
+        TcpConnection::pointer newConnection = TcpConnection::create(mAcceptor.get_io_service());
+        mAcceptor.async_accept(newConnection->socket(), boost::bind(&TcpServer::HandleAccept, this, newConnection, boost::asio::placeholders::error));
+    }
+
+    void HandleAccept(TcpConnection::pointer newConnection, const boost::system::error_code& error)
+    {
+        std::cout << "Handling accept" << std::endl;
+        if (!error)
+            newConnection->Start();
+
+        StartAccept();
+    }
+
+    tcp::acceptor mAcceptor;
+};
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    try
+    {
+        boost::asio::io_service io;
+        TcpServer server(io);
+        std::cout << "Running" << std::endl;
+        io.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
